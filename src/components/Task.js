1d2e3f71e9e47c4bf59fd57f8e1feea75c20784b
Fitz8 <liamfitzgerald@hotmail.co.uk>
2022-07-08 16:35:32 +0100

    Refactored task component ternary operators

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,27 +2,14 @@ const Task = ({ text, handleRemoval, handleOnChange, isDone }) => {
   return (
     <div id="task-container">
     {/* The ternary operator statements are used to change states depending on if the task is checked as done */}
-      {isDone ? (
-        <p style={{ color: "lightgreen" }}>{text}</p>
-      ) : (
-        <p style={{ color: "#EEEEEE" }}>{text}</p>
-      )}
-
-      <button onClick={handleRemoval}>Remove</button>
-      {isDone ? (
+        <p style={isDone ? ({color:"lightgreen"}) : ({color:"#EEEEEE"})}>{text}</p>
+        <button onClick={handleRemoval}>Remove</button>
         <input
           className="checkbox"
           type="checkbox"
           onChange={handleOnChange}
-          checked
+          checked = {isDone}
         />
-      ) : (
-        <input 
-          className="checkbox" 
-          type="checkbox" 
-          onChange={handleOnChange} 
-        />
-      )}
     </div>
   );
 };
